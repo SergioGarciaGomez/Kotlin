16f2fc236e1b459365e88bb54a9aa7261d267b8c
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-09-28 10:46:11 +0200

    Ejercicios T01

--- src/tema1/Ejercicio03.kt
+++ src/tema1/Ejercicio03.kt
@@ -2,9 +2,7 @@ package tema1
 
 /*
     @author Sergio García
-
     Ejercicio 03 - Tema 01
-
     Programa que dado un año guardado en una variable a, muestre true or false según sea bisiesto o no. Un año es bisiesto
     si es divisible por 4 pero no entre 100, o si es divisible por 400. SIN CONDICIONALES.
 */
@@ -13,6 +11,6 @@ fun main() {
 
     print("Introduce un año: ")
     val a = readLine()!!.toInt()
-    val esBisiesto:Boolean = ((a % 4 == 0) && (a % 400 == 0))
+    val esBisiesto:Boolean = ((a % 4 == 0) && (a % 100 != 0) || (a % 400 == 0))
     println(esBisiesto)
 }
