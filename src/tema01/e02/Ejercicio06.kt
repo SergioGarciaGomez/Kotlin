9e7de4baa3008deff3b80db7da8006b955030160
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-10-05 18:42:31 +0200

    Ejercicios 02 - Tema 01

--- src/tema01/e02/Ejercicio06.kt
+++ src/tema01/e02/Ejercicio06.kt
@@ -14,11 +14,25 @@ package tema01.e02
 
 fun main() {
 
-    var altura = 4
+    val h = 6 // altura
+    var espacios = h - 1 // la primera capa siempre serán h - 1 espacios, y -1 para cada capa extra de pirámide
+    var capa = 1
 
-    for (i:Int in 1..altura) {
-        var cadena = ""
-        for (j:Int in altura..i) cadena += " "
+    for (i:Int in 1..h) {
+
+        for (j :Int in 1..espacios){ // bucle que pone los espacios en blanco
+            print(" ")
+        }
+        espacios-- // cada vez que se repita los espacios necesarios serán -1 en relación con el anterior
+
+        for (j :Int in 1..capa){ // imprime lo que sería la parte central de la pirámide, hacia la izquierda
+            print(j)
+        }
+
+        for (j :Int in (capa - 1) downTo 1){ // imprime la parte derecha sin contar el centro
+            print(j)
+        }
+        capa++
+        println()
     }
-
 }
