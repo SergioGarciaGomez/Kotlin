0b49e7ec85d0eefdd491a2b459e67f4024a59c99
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-10-07 10:47:22 +0200

    Repaso Clase

--- src/objetos/Personaje.kt
+++ src/objetos/Personaje.kt
@@ -2,7 +2,7 @@ package objetos
 
 const val algo = "" // las constantes solo almacenan tipos primitivos
 
-open class Personaje {
+abstract class Personaje { // open por defecto
     var nombre: String = "Alguno"
         get() = field.uppercase()
     var vida: Int = 100
@@ -13,14 +13,27 @@ class Mago: Personaje() {
 
 }
 
+data class Item(var nombre: String, var cantidad: Int)
+
+val item = Item("Poti de vida", 10)
+
+enum class tipoItem(puntos: Int) {
+    COMUN(1), POCOCOMUN(3), RARO(5), EPICO(10), LEGENDARIO(50)
+}
+
 //class Personaje(var nombre: String, val vida: Int)
 
 fun main() {
 
-    val guerrero = Personaje()
-    guerrero.nombre = "Otro" //setter
-    println(guerrero.nombre) //getter
     val mago = Mago()
-    println(mago.nombre)
+    mago.nombre = "Otro" //setter
+    println(mago.nombre) //getter
+    println(mago.vida)
+    tipoItem.COMUN
+    println(tipoItem.values().size)
+
+   /* while (mago.vida > 0) {
+        println("$")
+    }*/
 
 }
---
+++ src/objetos/SIngleton.kt
@@ -0,0 +1,5 @@
+package objetos
+
+object Foo {
+
+}
